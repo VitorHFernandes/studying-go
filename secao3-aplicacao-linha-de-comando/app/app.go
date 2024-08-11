7c105a14768edddc90f5c03ca3eb58ed2a4414c6
Vítor H. Fernandes <vitorfernandesdetusch@gmail.com>
2024-08-11 00:38:45 -0300

    v6

---
+++ secao3-aplicacao-linha-de-comando/app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada. *cli.App é um ponteiro!
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e nomes de servidores na internet"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server)
+	}
+}
