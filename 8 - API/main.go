f1eae62cf60c8b9c0be70c5c15a0cb3632bc1cb0
Vítor H. Fernandes <vitorfernandesdetusch@gmail.com>
2024-09-19 18:37:55 -0300

    config

--- 8 - API/main.go
+++ 8 - API/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,9 +9,14 @@ import (
 )
 
 func main() {
+	config.Loading()
+
+	fmt.Println(config.StringConnectionDB)
+
+	fmt.Println(config.Port)
 	fmt.Println("Rodando API")
 
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
